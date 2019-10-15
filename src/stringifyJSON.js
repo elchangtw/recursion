6ef0d9ca4aca45b0e24a6a502b05641c9d4afa12
elchangtw <elchang.tw@gmail.com>
2019-10-15 08:50:23 +0800

    having functions and undefined gives bugs
    AssertionError: expected '{"functions":,"undefined":}' to equal '{}'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,70 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+    var result = '';
+
+    if(typeof obj==='undefined'){
+    }
+    else if(typeof obj==='function'){
+    }
+    //else if(typeof obj==='null'){
+    //    result += 'null';
+    //}
+    else if(obj===null){
+        result += 'null';
+    }
+
+    else if(typeof obj==='boolean'){
+        result += obj;
+    }
+
+    else if(typeof obj==='number'){
+        result += obj;
+    }
+    
+    else if(typeof obj==='string'){
+      result += '"' + obj + '"';
+    }
+
+    else if(typeof obj==='object'){
+        //array
+        if( Array.isArray(obj) ){
+
+            if( obj.length===0 ){
+              result += '[]';
+            }
+            else{
+              result += '[';
+              _.each(obj, function(element, index){
+                  result += stringifyJSON(element);
+                  result += ',';
+              });
+              if(result[result.length-1]===','){
+                  result = result.slice(0, result.length-1);
+              }
+              result += ']';
+            }
+
+            return result;
+        }
+        else if(obj instanceof Object){
+            result += '{';
+            _.each(obj, function(element, index){
+                result += stringifyJSON(index);
+                result += ':';
+                result += stringifyJSON(element);
+                result += ',';
+            });
+            if(result[result.length-1]===','){
+                result = result.slice(0, result.length-1);
+            }
+            result += '}';
+        }
+    }
+
+    else{
+    //    result += 'null';
+    }
+
+    return result;
 };
