95478f0e5461b22923e7c50f76bf29629bf2bd2e
elchangtw <elchang.tw@gmail.com>
2019-10-15 15:34:22 +0800

    started parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,24 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+    var obj = {};
+
+    if( typeof json !== string){
+        return ;
+    }
+
+    if( json === 'null' ){
+        return null;
+    }
+
+    //boolean
+    if( json === 'true' ){
+        return true;
+    }
+    if( json === 'false' ){
+        return false;
+    }
+
+    //if( json )
+    //if (text.matches("[0-9]+") && text.length() > 2) {
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,38 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-    var result = '';
+    //var result = '';
+    var result; //do not initialize as string?
     //var flag = 1;
 
     if(typeof obj==='undefined'){
         //flag = 0;
         return 'FAILUREFLAG';
+        //return result;
     }
 
     else if(typeof obj==='function'){
         //flag = 0;
         return 'FAILUREFLAG';
+        //return result;
     }
 
     else if(typeof obj==='boolean'){
+        //result += obj;
+        result = ''; //did not initialize as string
         result += obj;
     }
 
     else if(typeof obj==='number'){
+        result = ''; //did not initialize as string
         result += obj;
     }
     
     else if(typeof obj==='string'){
+        result = ''; //did not initialize as string
         result += '"' + obj + '"';
     }
 
     else if(obj===null){
+        result = ''; //did not initialize as string
         result += 'null';
     }
 
     else if(typeof obj==='object'){
         //array
         if( Array.isArray(obj) ){
+            result = ''; //did not initialize as string
 
             if( obj.length===0 ){
               result += '[]';
@@ -56,6 +65,7 @@ var stringifyJSON = function(obj) {
             return result;
         }
         else if(obj instanceof Object){
+            result = ''; //did not initialize as string
             result += '{';
             _.each(obj, function(element, index){
                 result += stringifyJSON(index);
@@ -77,8 +87,17 @@ var stringifyJSON = function(obj) {
 
     if( result.indexOf('FAILUREFLAG') > 0 ){
       //return result;
+      //return typeof result;
       return '{}';
     }
+
+    //return typeof result;
+    /*
+    if( typeof result !== 'string' ){
+      return 'ok: ' + typeof result;
+      //return '{}';
+    }
+    */
 
     return result;
 };
